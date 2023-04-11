56bff58847b5192cf1a283893e6b8ed9bdfc8b00
Jarod Girardin <jarod.girardin@he-arc.ch>
2023-04-11 18:27:55 +0200

    better randomizer

--- src/main.cpp
+++ src/main.cpp
@@ -1,35 +1,55 @@
 #include <iostream>
 #include <string>
 #include <raylib.h>
-#include "square.h"
 #include <vector>
 #include <time.h>
+#include <random>
+
 #include "colors.h"
+#include "square.h"
+#include "Button.h"
 
+// TODO: Sometimes a button is not there, outside of  the box ?
+Square* generateSquare(int minSize, int maxSize, int windowWidth, int windowHeight, std::mt19937 &generator) {
 
+    
+    // between minSize and maxSize
+    std::uniform_int_distribution<int> distSize(minSize, maxSize);
 
-Square* generateSquare(int minSize, int maxSize, int windowWidth, int windowHeight) {
+    int size = distSize(generator);
+    
+    
+    // position is whole grid
+    std::uniform_int_distribution<int> distPositionX(0, windowWidth - size);
+    std::uniform_int_distribution<int> distPositionY(0, windowHeight - size);
 
-    int size = minSize + std::rand() % (maxSize + 1 - minSize);
-
-    int posX = std::rand() % (windowWidth - size);
-    int posY = std::rand() % (windowHeight - size);
+    int posX = distPositionX(generator);
+    int posY = distPositionY(generator);
     Vector2 position = {(float)posX, (float)posY};
 
-    int dirX = std::rand() % (windowWidth - size);
-    int dirY = std::rand() % (windowHeight - size);
+
+    // direction is [1;3] and positive or negative
+    std::uniform_int_distribution<int> distDirection(0, 5);
+    std::uniform_int_distribution<int> distSign(1, 2);
+    
+    int dirX = distDirection(generator) * (distSign(generator) == 1 ? 1 : -1);
+    int dirY = distDirection(generator) * (distSign(generator) == 1 ? 1 : -1);
     Vector2 direction = {(float)dirX, (float)dirY};
-    direction = {1.0, 1.0};
-    Color color = colors[std::rand() % sizeof(colors)/sizeof(Color)];
+
+    // std::cout << dirX << " " << dirY << std::endl;
+    // choose a random color from the list
+    std::uniform_int_distribution<int> distColor(0, sizeof(colors)/sizeof(Color));
+    Color color = colors[distColor(generator)];
 
     Square* square = new Square(size, position, direction, color);
     return square;
 }
 
 int main(int argc, char **argv) {
-    
-    std::srand(time(NULL));
-    
+        
+    std::random_device device;
+    std::mt19937 generator(time(0));
+
     int windowWidth = 800;
     int windowHeight = 600;
 
@@ -40,9 +60,11 @@ int main(int argc, char **argv) {
 
     std::vector<Square*> squaresList;
 
-    squaresList.push_back(generateSquare(50, 200, windowWidth, windowHeight));
-    squaresList.push_back(generateSquare(50, 200, windowWidth, windowHeight));
-    squaresList.push_back(generateSquare(50, 200, windowWidth, windowHeight));
+
+    Button btn({0, 0}, {100, 80}, RED);
+    btn.onClick([&]() {
+            squaresList.push_back(generateSquare(50, 200, windowWidth, windowHeight, generator));
+        });
     
     while (!WindowShouldClose()) {
         BeginDrawing();
@@ -52,6 +74,8 @@ int main(int argc, char **argv) {
             p->draw();
             p->move(windowWidth, windowHeight);
         }
+        btn.draw();
+
         EndDrawing();
     }
 
