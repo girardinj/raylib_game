e40303def51e3be47d34612f13c7a1e24001d92a
Jarod Girardin <jarod.girardin@he-arc.ch>
2023-04-11 18:28:25 +0200

    dunno bro

--- src/Square.cpp
+++ src/Square.cpp
@@ -5,24 +5,24 @@ int Square::COUNTER = 0;
 
 Square::Square(double length, Vector2 position, Vector2 direction, Color color) {
     this->id = Square::COUNTER++;
-    this->length = length;
+    this->size = {(float)length, (float)length};
     this->position = position;
     this->direction = direction;
     this->color = color;
 }
 
 void Square::draw() {
-    DrawRectangle(this->position.x, this->position.y, this->length, this->length, this->color);
-    DrawText(std::to_string(this->id).c_str(), this->position.x, this->position.y, this->length, BLACK);
+    DrawRectangleV(this->position, this->size, this->color);
+    DrawText(std::to_string(this->id).c_str(), this->position.x, this->position.y, this->size.x, BLACK);
 }
 
 void Square::move(int rightBorder, int bottomBorder) {
     
-    if (this->position.x + this->length >= rightBorder - this->direction.x ||
+    if (this->position.x + this->size.x >= rightBorder - this->direction.x ||
       this->position.x + this->direction.x <= 0)
         this->direction.x *= -1;
     
-    if (this->position.y  + this->length >= bottomBorder - this->direction.y ||
+    if (this->position.y  + this->size.y >= bottomBorder - this->direction.y ||
       this->position.y + this->direction.y <= 0)
         this->direction.y *= -1;
 
--- src/Square.h
+++ src/Square.h
@@ -8,7 +8,7 @@ class Square {
         void draw();
         void move(int rightBorder, int bottomBorder);
     private:
-        double length;
+        Vector2 size;
         int id;
         Vector2 position;
         Vector2 direction;
